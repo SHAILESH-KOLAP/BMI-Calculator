613b8c67a3dffceb1d977c69eb27ab1c6ac7fcd2
Shailesh Kolap <skolap22@gmail.com>
2022-05-29 09:50:20 +0530

    Added get request for server

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-import express from "express";
+const express = require("express");
 const app = express();
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
 
 app.listen(3000, function () {
   console.log("Listening on Port 3000");
